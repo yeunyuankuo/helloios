36cf394e9ea5731718065f67035e3eea90048f79
Yeun-Yuan (Jessie) Kuo <kuoy2@uw.edu>
2017-10-03 21:54:09 -0700

    Hello-iOS

---
+++ Hello-iOS/HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Jessie Kuo on 03/10/2017.
+//  Copyright © 2017 appcat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
